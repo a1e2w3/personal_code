29d240cc3915f573732987031873285e6e7e4b02
wangcong07 <wangcong07@172.24.190.124>
2019-01-22 19:27:54 +0800

    fixed code style

--- wrpc/network/channel.cpp
+++ wrpc/network/channel.cpp
@@ -212,8 +212,8 @@ void Channel::destroy_response(IResponse* response) {
 #if WRPC_USE_CONTROLLER_INST_POOL_CAPACITY > 0
 // 使用对象池
 ControllerPtr Channel::create_controller() {
-	typedef common::InstancePool<Controller, ChannelPtr&&, const RPCOptions&> ControllerInstPool;
-	static ControllerInstPool* s_controller_pool = nullptr;
+    typedef common::InstancePool<Controller, ChannelPtr&&, const RPCOptions&> ControllerInstPool;
+    static ControllerInstPool* s_controller_pool = nullptr;
     static std::once_flag s_init_controller_pool_once;
     std::call_once(s_init_controller_pool_once, [&s_controller_pool] () -> void {
         s_controller_pool = new ControllerInstPool(
